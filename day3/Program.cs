222e4dd75116f25fa993501e0b2ece020a4d6738
Bishow73 <bihowkhatri73@gmail.com>
2023-03-28 09:23:18 +0545

    OOP in C#

---
+++ day3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using day3_1;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            person Bishow =new person();
+            Bishow.noofEyes = 2;
+            Bishow.hairColor = "Black";
+            Console.WriteLine("you have"+ Bishow.noofEyes+  " Eyes");
+            Console.WriteLine("you have" + Bishow.hairColor + "hair");
+            Bishow.EatingHabit();
+            Bishow.EatingHabit("chowin");
+            string mylunch = Bishow.EatingHabits("Biryani");
+            Console.WriteLine(mylunch);
+            Console.ReadKey();        
+        }
+    }
+}
---
+++ day3/person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace day3_1
+{
+    internal class person
+    {
+        int numberofEyes;
+        public int noofEyes { get { return numberofEyes; } set { numberofEyes = value; } }
+        public string hairColor { get; set; }
+        public void EatingHabit()
+        {
+            Console.WriteLine("I usally have tea in break");
+        }
+        public void EatingHabit(string mylunch)
+        {
+            Console.WriteLine("i usually have" + mylunch + "in lunch");
+                }
+        public string EatingHabits(string mylunch)
+        {
+            return "I usually have" + mylunch + "in my lunch";
+        }
+    }
+}
